b32eb189ea8635792e7e771f4f0fba4cebdb7b3a
Daniel Herrera <info.danielherrera@gmail.com>
2024-01-21 15:36:19 -0700

    Fix Aside

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -15,13 +15,13 @@ function Aside() {
         </div>
       </div>
       <div
-        className={`flex flex-col md:items-center md:pb-0 absolute md:static md:z-auto z-10 left-0 w-auto md:w-auto md:pl-0 pl-7 bg-gray-200 shadow-md md:shadow-none gap-4 transition-all duration-200 ease-in ${
+        className={`flex flex-col md:items-start md:pb-0 absolute md:static md:z-auto z-10 left-0 w-auto md:w-auto md:pl-0 pl-7 bg-gray-200 shadow-md md:shadow-none gap-4 transition-all duration-200 ease-in ${
           open ? "left-0 shadow-lg" : "left-[-200px]"
         }`}
       >
         <article className="categorias flex flex-col items-start mt-3 md:mt-4">
           <h2 className=" text-2xl font-semibold pb-3">Categorias</h2>
-          <ul className="text-sm flex flex-col items-start gap-1">
+          <ul className="accent-[#034494] text-sm flex flex-col items-start gap-1">
             <li>
               <input type="radio" name="categories" id="1" />
               <label htmlFor="1">All Products</label>
@@ -36,7 +36,7 @@ function Aside() {
             </li>
           </ul>
         </article>
-        <article className="categorias flex justify-between flex-col md:mt-7 pr-8 mb-5">
+        <article className="categorias flex flex-col items-start md:mt-7 mr-2 mb-5">
           <h2 className="text-2xl font-semibold pb-3">Precio</h2>
           <ul className="text-xl flex justify-between gap-1">
             <li className="flex flex-col w-full gap-3">
@@ -44,7 +44,7 @@ function Aside() {
                 <span className="text-sm">$ 20 MXN</span>
                 <span className="text-sm">$ 2349 MXN</span>
               </div>
-              <input type="range" name="categories" id="1" />
+              <input className="accent-[#034494]" type="range" name="categories" id="1" />
             </li>
           </ul>
         </article>
