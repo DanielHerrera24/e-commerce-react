95b762c05760761e022fd26c91f8e1c5715b5f12
Daniel Herrera <info.danielherrera@gmail.com>
2024-01-25 23:06:05 -0700

    Loader

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { getProducts } from "../../serverMock/productMock";
 import CardComp from "../Layout/Cards/Cards";
+import { ScaleLoader } from "react-spinners";
 
 function ItemListContainer({ greeting }) {
   const [productos, setProductos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const res = await getProducts();
         setProductos(res);
+        // Simulación de carga
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
       } catch (err) {
         console.log(err.message);
       }
@@ -22,13 +28,17 @@ function ItemListContainer({ greeting }) {
       <div className="flex justify-center text-center cont-title pb-6">
         <h1 className="text-3xl font-semibold">{greeting}</h1>
       </div>
-      <div className="listItems cont-cards grid grid-cols-2 sm:grid-cols-3 sm:gap-3 gap-1">
-        {productos.map((producto) => {
-          return (
+      {isLoading ? (
+        <div className="flex justify-center items-center h-40">
+          <ScaleLoader color="#034494" />
+        </div>
+      ) : (
+        <div className="listItems cont-cards grid grid-cols-2 sm:grid-cols-3 sm:gap-3 gap-1">
+          {productos.map((producto) => (
             <CardComp key={producto.id} producto={producto} />
-          )
-        })}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
